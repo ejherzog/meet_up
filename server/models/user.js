892a778959a9c2e89e11ad98ac475542d107fe82
Eva Herzog <evajherzog@gmail.com>
2018-04-12 10:09:45 -0400

    updating user schema and methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 let userSchema = new Schema({
 
@@ -12,3 +13,35 @@ let userSchema = new Schema({
   }]
 
 });
+
+userSchema.statics.addUser = function (email, name, password) {
+
+  let newUser = new this({
+    email: email,
+    name: name,
+    password: password
+    availability: [];
+  });
+  return bcrypt.hash(newUser.password, 1).then((hash) => {
+    newUser.password = hash;
+    return newUser.save();
+  });
+
+}
+
+userSchema.statics.check = function (email, password) {
+  // determines if a given password for a username is valid  or not.
+  // find a user with the username equivalent to the username passed in.
+  // if  there is no  user then throw a new  Error('No User') else  return the result
+  // of bcrypt.compare for the password and the user's password
+  // STUB
+  return this.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        throw new Error('No user with that email');
+      } else {
+        return bcrypt.compare(password, user.password);
+      }
+    });
+  // ENDSTUB
+};
